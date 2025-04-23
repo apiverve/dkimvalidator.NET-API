28343456e30a5409cbc9edb3bacc46ae99237be6
APIVerve Team <hello@apiverve.com>
2025-04-23 11:23:20 -0500

    Bug Fixes

---
+++ DKIMValidatorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class DKIMValidatorQueryOptions {
+public string domain { get; set; }
+}
+
+}
